b98a728cb2f4f82471141d7a81deac82751a710e
Rassul-creator-commits <181149570+Rassul-creator-commits@users.noreply.github.com>
2024-09-17 15:00:30 -0400

    Add files via upload

---
+++ Khalizov_Rassul_Lecture 2.cpp
@@ -0,0 +1,31 @@
+//Rassul Khalizov Lecture 2
+//9/16/24
+
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main()
+{
+    //the names of the variables
+    int banana;              //Whole numbers
+    float apple;             // decimals
+    double kiwi;            // Float large numbers
+    bool pineapple;         //true statements
+    char watermelon;       // can only hold one character 
+    string strung;        // sequence of characters in double quotation marks
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
